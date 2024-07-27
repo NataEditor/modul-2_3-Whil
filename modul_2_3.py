dac0583baff930576c7694aa269cb6313b5bce54
NataEditor <nako_al@mail.ru>
2024-07-27 23:53:59 +0300

    Add files via upload
    Работа с циклам While

---
+++ modul_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print(my_list)
+d = len(my_list)
+#print(d)
+nambe = 0
+while nambe <= d:
+    if my_list[nambe] >= 0:
+        print(my_list[nambe])
+        nambe = nambe + 1
+    else:
+        print("stop")
+        break
